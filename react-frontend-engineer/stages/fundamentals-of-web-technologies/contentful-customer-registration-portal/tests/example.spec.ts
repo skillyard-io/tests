6cb105830f9c0fe263fb8cf37d2f46da66af8e0e
Frantz Kati <bahdcoder@gmail.com>
2023-02-02 05:46:06 +0100

    chore: setup playwright reporters

--- react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/tests/example.spec.ts
+++ react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/tests/example.spec.ts
@@ -11,7 +11,7 @@ test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get startedx' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
