fc12f4d58472ab02214a4f2efccbc8a804fdfca9
Frantz Kati <bahdcoder@gmail.com>
2023-02-02 14:44:56 +0100

    chore: reduce timeout and add some failing tests

--- react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/playwright.config.ts
+++ react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 6 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['list'], ['json', {  outputFile: 'skillyard-io-playwright-test-results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
