192d7b57024d93ad7d5378ab93fd3fcf543df939
Frantz Kati <bahdcoder@gmail.com>
2023-02-03 01:41:03 +0100

    chore: make tests pass

--- react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/playwright.config.ts
+++ react-frontend-engineer/stages/fundamentals-of-web-technologies/contentful-customer-registration-portal/playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['list'], ['json', {  outputFile: 'skillyard-io-playwright-test-results.json' }]],
+  reporter: [['list'], ['html', {outputFolder: 'skillyard-io-playwright-test-results', open: 'never'}], ['json', {  outputFile: 'skillyard-io-playwright-test-results.json' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
